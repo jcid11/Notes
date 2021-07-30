dcfb3d39097c3b7694630f6699592ccc43de4b7a
Jorge <jorge.cc91@gmail.com>
2021-07-29 23:17:21 -0400

    test commit

--- lib/utilities/notes_model.dart
+++ lib/utilities/notes_model.dart
@@ -5,7 +5,7 @@ final String title;
 final String note;
 final List list;
 final Timestamp date;
-Timestamp date2;
+
 NoteModel({this.title,this.note,this.list,this.date});
   Map<String, dynamic> toMap2() {
     return {
